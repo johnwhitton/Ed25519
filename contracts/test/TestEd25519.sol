33a5f0c11c6d3e468b87f7f23ec188fbd1a09516
John Whitton <john@johnwhitton.com>
2022-12-08 18:11:16 -0800

    Add logging for gas

--- contracts/test/TestEd25519.sol
+++ contracts/test/TestEd25519.sol
@@ -5,12 +5,27 @@ pragma experimental ABIEncoderV2;
 import "../libraries/Ed25519.sol";
 
 contract TestEd25519 {
+    bool verified;
+
     function verify(
         bytes32 k,
         bytes32 r,
         bytes32 s,
         bytes memory m
     ) public pure returns (bool) {
+        // bool verified = Ed25519.verify(k, r, s, m);
+        // return verified;
         return Ed25519.verify(k, r, s, m);
+    }
+
+    function verifySet(
+        bytes32 k,
+        bytes32 r,
+        bytes32 s,
+        bytes memory m
+    ) public returns (bool) {
+        // bool verified = Ed25519.verify(k, r, s, m);
+        // return verified;
+        verified = Ed25519.verify(k, r, s, m);
     }
 }
--- test/Ed25519.spec.ts
+++ test/Ed25519.spec.ts
@@ -14,10 +14,33 @@ describe('Ed25519', () => {
         ed25519 = (await ed25519Factory.deploy()) as TestEd25519
     })
 
+    // for (const { description, pub, msg, sig, valid } of require('./ed25519-test1.json')) {
+    //     it(description, async () => {
+    //         const [r, s] = [sig.substring(0, 64), sig.substring(64)];
+    //         expect(valid).to.eq(await ed25519.verify(`0x${pub}`, `0x${r}`, `0x${s}`, `0x${msg}`))
+    //    });
+    // }
+
+    // for (const { description, pub, msg, sig, valid } of require('./ed25519-test2.json')) {
+    //     it(description, async () => {
+    //         const [r, s] = [sig.substring(0, 64), sig.substring(64)];
+    //         expect(valid).to.eq(await ed25519.verify(`0x${pub}`, `0x${r}`, `0x${s}`, `0x${msg}`))
+    //    });
+    // }
+
+    // for (const { description, pub, msg, sig, valid } of require('./ed25519-test3.json')) {
+    //     it(description, async () => {
+    //         const [r, s] = [sig.substring(0, 64), sig.substring(64)];
+    //         expect(valid).to.eq(await ed25519.verify(`0x${pub}`, `0x${r}`, `0x${s}`, `0x${msg}`))
+    //    });
+    // }
+
     for (const { description, pub, msg, sig, valid } of require('./ed25519-tests.json')) {
         it(description, async () => {
             const [r, s] = [sig.substring(0, 64), sig.substring(64)];
-            expect(valid).to.eq(await ed25519.verify(`0x${pub}`, `0x${r}`, `0x${s}`, `0x${msg}`))
+            const tx = await ed25519.verifySet(`0x${pub}`, `0x${r}`, `0x${s}`, `0x${msg}`)
+            const receipt = await tx.wait();
+            console.log(`cumulativeGasUsed: ${receipt.cumulativeGasUsed.toString()} ${description}`)
        });
     }
 })
